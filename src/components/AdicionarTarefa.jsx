eecdcc72a4cb4b7233500224e857eb594c7aed36
Eliel Souza <elielsf13@gmail.com>
2024-03-22 18:00:03 -0300

    modificar componentes

--- src/components/AdicionarTarefa.jsx
+++ src/components/AdicionarTarefa.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
+import NovaTarefa from './NovaTarefa.jsx'
 import '../css/AdicionarTarefa.css'
 
 export default function AdicionarTarefa() {
-    //const[novaTarefa, setNovaTarefa] = useState;
-    const novaTarefa = () => {
-        console.log('a');
+    const [criarTarefa, setCriarTarefa] = useState(false);
+    const handleClick = () => {
+        setCriarTarefa(!criarTarefa);
     }
 
     return (
         <div className='containerButton'>
-            <button onClick='novaTarefa'>a</button>
+            <button onClick={handleClick}>Nova tarefa</button>
+            {criarTarefa && <NovaTarefa />}
         </div>
     )
 }
---
+++ src/components/NovaTarefa.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NovaTarefa() {
+    return (
+        <div>
+            <p>adsa</p>
+        </div>
+    )
+}
